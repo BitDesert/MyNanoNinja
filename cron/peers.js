80f7c9a4bdf6e4174ec79d4967c26ac9c3948868
Tobias Palmer <tobiaspalmer93@gmail.com>
2019-07-24 17:05:06 +0200

    peers regex

--- cron/peers.js
+++ cron/peers.js
@@ -1,7 +1,10 @@
 var cron = require('node-cron');
+var async = require("async");
 var maxmind = require('maxmind');
 var geo_asn = maxmind.openSync('./utils/GeoLite2-ASN.mmdb');
 var geo_city = maxmind.openSync('./utils/GeoLite2-City.mmdb');
+
+const regex_ip = /\[::ffff:([0-9.]+)\]:[0-9]+/
 
 const {
   Nano
@@ -14,7 +17,7 @@ const node = new Nano({
 var Account = require('../models/account');
 
 function updatePeers() {
-  console.log('Updating Peers...');
+  console.log('== Updating Peers...');
 
   node.rpc('confirmation_quorum', {
     peer_details: true
@@ -22,49 +25,72 @@ function updatePeers() {
     .then(response => {
       if (!response) return;
 
-      for (const peer of response.peers) {
-        updatePeer(peer)
-      }
+      async.forEachOfSeries(response.peers, (peer, key, callback) => {
+
+        var match = regex_ip.exec(peer.ip)
+        
+        if (match) {
+            updatePeer(peer, match[1], callback)
+        } else {
+          callback()
+        }
+
+      }, err => {
+        if (err) {
+          console.error(err.message);
+          return
+        }
+        console.log('== Peers updated.');
+      });
     })
     .catch(reason => {
       console.error(reason);
     });
 }
 
-function updatePeer(peer) {
+function updatePeer(peer, ip, callback) {
   Account.findOne(
     {
       'account': peer.account
     }, function (err, account) {
-      if (err || !account) return;
+      if (err || !account){
+        callback()
+        return;
+      }
 
       try {
-        account.network.ip = peer.ip;
+        account.network.ip = ip;
 
-        var geo_asn_response = geo_asn.get(peer.ip);
+        var geo_asn_response = geo_asn.get(ip);
 
         if(geo_asn_response && geo_asn_response.autonomous_system_organization){
           account.network.provider = geo_asn_response.autonomous_system_organization;
         }
 
-        var geo_city_response = geo_city.get(peer.ip);
+        var geo_city_response = geo_city.get(ip);
 
-        account.location.country = geo_city_response.country.iso_code;
-
-        if(geo_city_response.city){
-          account.location.city = geo_city_response.city.names.en;
+        if(geo_city_response){
+          account.location.country = geo_city_response.country.iso_code;
+  
+          if(geo_city_response.city){
+            account.location.city = geo_city_response.city.names.en;
+          }
+  
+          account.location.latitude = geo_city_response.location.latitude;
+          account.location.longitude = geo_city_response.location.longitude;
+        } else {
+          console.log('No city for ' + peer.ip + ' / ' + peer.account)
         }
-
-        account.location.latitude = geo_city_response.location.latitude;
-        account.location.longitude = geo_city_response.location.longitude;
-
+        
         account.save(function (err) {
           if (err) {
             console.log("CRON - updatePeer - Error saving account", err);
           }
+          callback()
         });
       } catch (error) {
         console.error(error, peer);
+        callback()
       }
     });
 }
