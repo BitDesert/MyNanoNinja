e61710065efa4a0518dbefb06e281fa11b185dce
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-05-30 13:11:14 +0200

    allow Authorization

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ module.exports = function (nanorpc) {
 
   router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
