257478ffce112aa4705c638c8d4820f39c66430a
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-07-10 23:45:54 +0200

    node api payment_api

--- routes/api/node.js
+++ routes/api/node.js
@@ -10,7 +10,7 @@ const client = new NanoClient({url: process.env.NODE_RPC})
 var router = express.Router();
 
 const opts = {
-  points: 240,
+  points: 500,
   duration: 60 * 60, // in seconds
 };
 
@@ -64,7 +64,7 @@ const allowed_actions = [
 isApiAuthorized = (req, res, next) => {
   const authHeader = req.headers.authorization
 
-  const consumePoints = req.body.action === 'work_generate' ? 10 : 1;  
+  const consumePoints = req.body.action === 'work_generate' ? 50 : 1;  
 
   if (!authHeader) {
     console.log('NODE API - No header')
@@ -116,6 +116,7 @@ router.get('/', function (req, res, next) {
     loggedin: req.isAuthenticated(),
     title: 'Nano Node API',
     user: req.user,
+    payment_api: process.env.PAYMENT_API,
   });
 });
 
@@ -203,7 +204,7 @@ router.get('/payment/:token/verify', isLoggedIn, function (req, res) {
   var output = {};
 
   request.get({
-    url: 'https://mynano.ninja/payment/api/verify?token=' + token,
+    url: process.env.PAYMENT_API + '/verify?token='+token,
     json: true
   }, function (err, response, data) {
     if (err || response.statusCode !== 200) {
