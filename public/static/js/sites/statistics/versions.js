c67ceb5691fe38a052da7a51b4881f60e88b12f5
BitDesert <tobiaspalmer93@gmail.com>
2020-07-17 09:27:57 +0200

    version doughnut

--- public/static/js/sites/statistics/versions.js
+++ public/static/js/sites/statistics/versions.js
@@ -8,5 +8,55 @@ init.push(getData);
 
 function getData() {
   $.get("/api/telemetry/raw", function (data) {
+    var counts = data.metrics.reduce((p, c) => {
+      var name = c.major_version + '.' + c.minor_version + '.' + c.patch_version;
+      if (!p.hasOwnProperty(name)) {
+        p[name] = 0;
+      }
+      p[name]++;
+      return p;
+    }, {});
+    
+    prepareGraphData(counts);
+  });
+}
+
+function prepareGraphData(data){
+  var chartdata = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Versions',
+        data: [],
+        backgroundColor: []
+      }
+    ]
+  };
+
+  var colorcount = 0;
+  for (const [key, value] of Object.entries(data)) {
+    console.log(key, value);
+
+    chartdata.labels.push(key);
+    chartdata.datasets[0].data.push(value);
+    chartdata.datasets[0].backgroundColor.push(chartColors[colorcount]);
+    colorcount++;
+  }
+
+  setupGraph(chartdata);
+}
+
+function setupGraph(data) {
+  var ctx = document.getElementById('chartcanvas').getContext('2d');
+  var myLineChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {
+      responsive: true,
+      cutoutPercentage: 0,
+      legend: {
+        display: true
+      }
+    }
   });
 }
