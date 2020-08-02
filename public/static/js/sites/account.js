1c9f67e247a3a05cbddf387e78e419030cbf821f
Tobias Palmer <tobiaspalmer93@gmail.com>
2020-08-02 13:14:57 +0200

    fix account status

--- public/static/js/sites/account.js
+++ public/static/js/sites/account.js
@@ -28,13 +28,15 @@ var app = new Vue({
       .then(response => {
         this.account = response.data;
         this.account.isOnline = moment(this.account.lastVoted).add(1, 'day').isAfter(/*now*/)
-        if(moment(accounts[i].lastVoted).add(10, 'minutes').isAfter(/*now*/)){
+        if(moment(this.account.lastVoted).add(10, 'minutes').isAfter(/*now*/)){
           this.account.status = 'online';
-        } else if(moment(accounts[i].lastVoted).add(1, 'day').isAfter(/*now*/)) {
+        } else if(moment(this.account.lastVoted).add(1, 'day').isAfter(/*now*/)) {
           this.account.status = 'unstable';
         } else {
           this.account.status = 'offline';
         }
+        console.log('STATUS', this.account.status);
+        
 
         if (response.data.alias)
           this.editAccount.account_alias = response.data.alias;
@@ -45,16 +47,20 @@ var app = new Vue({
         if (response.data.website)
           this.editAccount.account_website = response.data.website;
 
-        if (response.data.server.type)
-          this.editAccount.server_type = response.data.server.type;
-
-        if (response.data.server.cpu)
-          this.editAccount.server_cpu = response.data.server.cpu;
-
-        if (response.data.server.ram)
-          this.editAccount.server_ram = response.data.server.ram;
+        if (response.data.server){
+          if (response.data.server.type)
+            this.editAccount.server_type = response.data.server.type;
+  
+          if (response.data.server.cpu)
+            this.editAccount.server_cpu = response.data.server.cpu;
+  
+          if (response.data.server.ram)
+            this.editAccount.server_ram = response.data.server.ram;
+        }
       })
-      .catch(reason => { });
+      .catch(reason => {
+        console.error(reason);
+      });
 
     axios
       .get('/api/accounts/' + this.address + '/history')
