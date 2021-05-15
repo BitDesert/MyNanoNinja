16abe0f6fe9f9e1ffdb7eaafedb2afb80f278398
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-05-15 12:03:07 +0200

    formatting

--- cron/telemetry.js
+++ cron/telemetry.js
@@ -4,9 +4,9 @@ var tools = require('../tools');
 var nanorpc = require('../nano/rpc_client');
 
 const NanoClient = require('nano-node-rpc');
-const client = new NanoClient({url: process.env.NODE_RPC})
+const client = new NanoClient({ url: process.env.NODE_RPC })
 
-async function updateTelemetry(){
+async function updateTelemetry() {
   var quorum = await client._send('confirmation_quorum', {
     peer_details: true
   })
@@ -24,22 +24,22 @@ async function updateTelemetry(){
       port
     })
 
-    if(telemetry.error){
+    if (telemetry.error) {
       console.log('TELEMETRY: No telemetry from', peer.account, address, telemetry.error);
       return
     }
-    
-    var account = await Account.findOne({account: peer.account})
+
+    var account = await Account.findOne({ account: peer.account })
 
     try {
       account.monitor.version = telemetry.major_version + '.' + telemetry.minor_version + '.' + telemetry.patch_version
       account.monitor.blocks = telemetry.block_count
-  
+
       account.monitor.sync = tools.round((telemetry.block_count / nanorpc.getBlockcount()) * 100, 3)
       if (account.monitor.sync > 100) {
         account.monitor.sync = 100
       }
-      
+
       account.telemetry.block_count = telemetry.block_count
       account.telemetry.cemented_count = telemetry.cemented_count
       account.telemetry.major_version = telemetry.major_version
@@ -47,14 +47,14 @@ async function updateTelemetry(){
       account.telemetry.patch_version = telemetry.patch_version
       account.telemetry.protocol_version = telemetry.protocol_version
       account.telemetry.peer_count = telemetry.peer_count
-      
+
       //console.log('TELEMETRY: OK from ', peer.account, telemetry.block_count);
     } catch (error) {
       console.log('TELEMETRY: Error at ', peer.account, error);
     }
 
     account.save()
-  });  
+  });
 }
 
 cron.schedule('*/15 * * * *', updateTelemetry);
